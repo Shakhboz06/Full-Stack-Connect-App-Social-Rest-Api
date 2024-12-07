1813dd4e9113391a73329c2f37c4ce0f316624e5
Shakhboz Shodiev <shakhboz.shodiev2001@gmail.com>
2024-12-07 23:55:47 +0100

    my stack

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
+  devServer:{
+    port: 3000
+  },
   app:{
     pageTransition: {name: "fade", mode: "out-in" }
   },
